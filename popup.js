af8d01ba88fa30bbfa543342ea3135e633a2bb4d
EruditePig <wagalimaxida@qq.com>
2016-12-01 23:21:48 +0800

    1、禁用了WSJ相关的功能，因为不再用了 2、测试了扩展存储相关的功能

--- popup.js
+++ popup.js
@@ -1,13 +1,40 @@
-// chrome.tabs.getSelected(null, function(tab){
-//         //alert(tab.url);
-//     $.get("http://www.baidu.com",function(data,status){
-//     	alert("data:" + data);
-//     });
-// });
-$(document).ready(function(){
-  $("button").click(function(){
-    $.get("082916communistparty4/",function(data,status){
-      alert("数据：" + data + "\n状态：" + status);
+
+function onClickRead()
+{
+    chrome.storage.local.get("count", function(result){
+        if(result.count == null)
+        {
+            alert("不存在这个变量");
+            return;
+        }
+        else
+        {
+            alert(result.count);
+        }
     });
-  });
-});
+}
+
+function onClickAdd()
+{
+    chrome.storage.local.get("count", function(result){
+        if(result.count == null)
+        {
+            chrome.storage.local.set({"count" : 1});
+        }
+        else
+        {
+            chrome.storage.local.set({"count" : parseInt(result.count) + 1});
+        }
+    });
+    
+    
+}
+
+window.onload = function()
+{
+    var btnRead = document.getElementById("read_count");
+    btnRead.onclick = onClickRead;
+    
+    var btnAdd = document.getElementById("add_count");
+    btnAdd.onclick = onClickAdd;
+};
