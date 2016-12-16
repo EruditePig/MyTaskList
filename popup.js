6681fa8567b87cf75a78805ad1df7492d2859833
277013083@qq.com <Z359jjnZ>
2016-12-16 10:32:13 +0800

    完善同步书签功能，并将书签也保存在本地。

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 
 window.onload = function()
 {
-    window.localTest = true;   // 是否本地测试
+    window.localTest = false;   // 是否本地测试
     
     // ************ 书签同步相关 ********************
     window.bookmarkUrl = window.localTest ? "http://localhost/bookmarks/bookmarks.php" : "http://sjxphp56.applinzi.com/bookmarks/bookmarks.php";
@@ -101,6 +101,16 @@ function addBookmark()
 
 function getLatestBookmarks()
 {
+    // 书签同步过程中需要用到的状态变量
+    // 记录本地最新书签时间
+    var localBookmarkTime;
+    // 记录服务器最新书签时间
+    var serverBookmarkTime;
+    // 服务器最新书签
+    var serverBookmarks;
+    // 书签栏ID
+    var bookmarkBarID;
+    
     // 从服务器上获取最新书签时间
     var getServerBookmarkTime = function()
     {
@@ -124,8 +134,6 @@ function getLatestBookmarks()
         return dfrd.promise();
     };
     
-    // 记录服务器最新书签时间
-    var serverBookmarkTime;
     
     // 从本地扩展获取时间
     var getLocalBookmarkTime = function()
@@ -143,14 +151,12 @@ function getLatestBookmarks()
         return dfrd.promise();
     };
     
-    // 记录本地最新书签时间
-    var localBookmarkTime;
     
     // 比较本地和服务器的时间
     var compareTime = function()
     {
          var dfrd = $.Deferred();
-         if(parseInt(localBookmarkTime) < parseInt(serverBookmarkTime))
+         if(typeof(localBookmarkTime) == "undefined" || parseInt(localBookmarkTime) < parseInt(serverBookmarkTime))
          {
              dfrd.resolve();
          }
@@ -161,8 +167,6 @@ function getLatestBookmarks()
          return dfrd.promise();
     }
     
-    // 服务器最新书签
-    var serverBookmarks;
     
     // 获取服务器最新书签
     var getServerBookmarks = function()
@@ -220,8 +224,9 @@ function getLatestBookmarks()
         return dfrd.promise();
     }
     
+    
     // 删除书签栏上所有的书签
-    var delAllBookmarksInBookmarkBar = function(bookmarkBarID)
+    var delAllBookmarksInBookmarkBar = function()
     {
         var dfrd = $.Deferred();
         chrome.bookmarks.getSubTree(bookmarkBarID, function(results)
@@ -244,6 +249,88 @@ function getLatestBookmarks()
                 });
         });
         return dfrd.promise();
+    };
+    
+        
+    // 将书签列表转化为树状文件夹的形式
+    function transBookmarksFromArray2FolderTree(bm, bmFolder, bmNoFolder)
+    {
+        var tagContainer = {};
+        // 循环所有书签，给所有tag计数
+        for (var i=0; i<bm.length; ++i)
+        {
+            for (var j=0; j<bm[i].tag.length; ++j)
+            {
+                if (tagContainer.hasOwnProperty(bm[i].tag[j]))
+                {
+                    tagContainer[bm[i].tag[j]] += 1;
+                }
+                else
+                {
+                    tagContainer[bm[i].tag[j]] = 1;
+                }
+            }
+        }
+        // 循环所有书签，根据自己tag中的最多的那个，决定如何分配
+        for(item in tagContainer)
+        {
+            if(tagContainer[item] > 1)
+            {
+                bmFolder[item] = [];
+            }
+        }
+        
+        for(var i=0; i<bm.length; ++i)
+        {
+            if(bm[i].tag.length == 0)
+            {
+                bmNoFolder.push(bm[i]);
+            }
+            else
+            {
+                var maxCount = 0;
+                var maxTag;
+                for(var j=0; j<bm[i].tag.length; ++j)
+                {
+                    if(tagContainer[bm[i].tag[j]] > maxCount)
+                    {
+                        maxCount = tagContainer[bm[i].tag[j]];
+                        maxTag = bm[i].tag[j];
+                    }
+                }
+                if(maxCount == 1)
+                {
+                    bmNoFolder.push(bm[i]);
+                }
+                else
+                {
+                    bmFolder[maxTag].push(bm[i]);
+                }
+            }
+        }
+    }
+    // 重建书签栏
+    var refillBookmarkBar = function()
+    {
+        var objBookmarks = JSON.parse(serverBookmarks);   // 书签字符串转对象
+        var bmFolder = {};
+        var bmNoFolder = [];
+        transBookmarksFromArray2FolderTree(objBookmarks, bmFolder, bmNoFolder);     // 将书签列表转化为树状文件夹的形式
+        for(item in bmFolder)
+        {
+            if(bmFolder[item].length == 0) continue;
+            chrome.bookmarks.create({"parentId" : bookmarkBarID, 'title': item},function(newFolder)
+            {
+                for(var j=0; j<bmFolder[newFolder.title].length; ++j)
+                {
+                    chrome.bookmarks.create({"parentId" : newFolder.id, "title" : bmFolder[newFolder.title][j].name, "url" : bmFolder[newFolder.title][j].url});}
+                }
+            );
+        }
+        for(var i=0; i<bmNoFolder.length; ++i)
+        {
+            chrome.bookmarks.create({"parentId" : bookmarkBarID, "title" : bmNoFolder[i].name, "url" : bmNoFolder[i].url});
+        }
     };
     
     $.when(getServerBookmarkTime())    // 从服务器上获取最新书签时间
@@ -271,133 +358,12 @@ function getLatestBookmarks()
             return saveBookmarks(bookmarks);
         })
     .then(getBookmarkBarID)  // 获取书签栏ID
-    .then(delAllBookmarksInBookmarkBar) // 删除书签栏所有书签
-    .done(function(){alert("书签删除完毕");})
+    .then(
+        function(barID)
+        {
+            bookmarkBarID = barID;
+            delAllBookmarksInBookmarkBar();
+        }) // 删除书签栏所有书签
+    .done(refillBookmarkBar)    // 重置书签栏
     .fail(function(msg){alert(msg);})
-}
-
-// 根据书签数据生成书签
-function refillBookmarkBar(bookmarks)
-{
-    var bookmarksBarId;     // 书签栏文件夹的ID
-    chrome.bookmarks.getTree(function(results) 
-    {
-        bookmarksBarId = results[0].children[0].id;     // 获取书签栏文件夹的ID
-        chrome.bookmarks.getSubTree(bookmarksBarId, function(results)
-        {
-            // 读取书签栏根目录下所有书签并删除
-            for(var i=0; i<results[0].children.length; ++i)
-            {
-                chrome.bookmarks.removeTree(results[0].children[i].id);
-            }
-        });
-    });
-            // 定时器等根目录下书签删除干净了之后再添加
-            var timeID = setInterval(function()
-            {
-                // 检测是不是书签栏根目录下删除干净了
-                chrome.bookmarks.getSubTree(bookmarksBarId, function(results)
-                {
-                    if(results[0].children.length == 0)     // 删干净了
-                    {
-                        clearInterval(timeID);      // 停止计时器
-                        chrome.storage.local.set({"time" : time});      // 往扩展里存储本次书签对应的时间
-                        chrome.storage.local.set({"bookmarks" : bookmarks});    // 往扩展里存储本次书签
-                        // 开始添加书签 
-                        var objBookmarks = JSON.parse(bookmarks);   // 书签字符串转对象
-                        var bmFolder = {};
-                        var bmNoFolder = [];
-                        transBookmarksFromArray2FolderTree(objBookmarks, bmFolder, bmNoFolder);     // 将书签列表转化为树状文件夹的形式
-                        for(item in bmFolder)
-                        {
-                            if(bmFolder[item].length == 0) continue;
-                            chrome.bookmarks.create({"parentId" : bookmarksBarId, 'title': item},function(newFolder)
-                            {
-                                for(var j=0; j<bmFolder[newFolder.title].length; ++j)
-                                {
-                                    chrome.bookmarks.create({"parentId" : newFolder.id, "title" : bmFolder[newFolder.title][j].name, "url" : bmFolder[newFolder.title][j].url});}
-                                }
-                            );
-                        }
-                        for(var i=0; i<bmNoFolder.length; ++i)
-                        {
-                            chrome.bookmarks.create({"parentId" : bookmarksBarId, "title" : bmNoFolder[i].name, "url" : bmNoFolder[i].url});
-                        }
-                    }
-                });
-            }, 300); 
-}
-
-// 将书签列表转化为树状文件夹的形式
-function transBookmarksFromArray2FolderTree(bm, bmFolder, bmNoFolder)
-{
-    var tagContainer = {};
-    // 循环所有书签，给所有tag计数
-    for (var i=0; i<bm.length; ++i)
-    {
-        for (var j=0; j<bm[i].tag.length; ++j)
-        {
-            if (tagContainer.hasOwnProperty(bm[i].tag[j]))
-            {
-                tagContainer[bm[i].tag[j]] += 1;
-            }
-            else
-            {
-                tagContainer[bm[i].tag[j]] = 1;
-            }
-        }
-    }
-    // 循环所有书签，根据自己tag中的最多的那个，决定如何分配
-    for(item in tagContainer)
-    {
-        if(tagContainer[item] > 1)
-        {
-            bmFolder[item] = [];
-        }
-    }
-    
-    for(var i=0; i<bm.length; ++i)
-    {
-        if(bm[i].tag.length == 0)
-        {
-            bmNoFolder.push(bm[i]);
-        }
-        else
-        {
-            var maxCount = 0;
-            var maxTag;
-            for(var j=0; j<bm[i].tag.length; ++j)
-            {
-                if(tagContainer[bm[i].tag[j]] > maxCount)
-                {
-                    maxCount = tagContainer[bm[i].tag[j]];
-                    maxTag = bm[i].tag[j];
-                }
-            }
-            if(maxCount == 1)
-            {
-                bmNoFolder.push(bm[i]);
-            }
-            else
-            {
-                bmFolder[maxTag].push(bm[i]);
-            }
-        }
-    }
-}
-
-function createBookmark()
-{
-    //var parentID;
-    //chrome.bookmarks.create({"parentId" : bookmarkBar.id, 'title': 'Extension bookmarks'},function(newFolder) {
-    //    chrome.bookmarks.create({"parentId" : newFolder.id, "title" : "baidu", "url" : "http://www.baidu.com"});}
-    //);
-    var bookmarksBarId;
-    chrome.bookmarks.getTree(function(results) 
-    {
-        bookmarksBarId = results[0].children[0].id;
-        chrome.bookmarks.create({"parentId" : bookmarksBarId, 'title': 'Extension bookmarks'},function(newFolder) {
-            chrome.bookmarks.create({"parentId" : newFolder.id, "title" : "baidu", "url" : "http://www.baidu.com"});}
-        );
-    });
 }
